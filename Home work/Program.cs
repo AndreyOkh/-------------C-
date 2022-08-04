4a95f0b6d8f7bf655ff28129d81376edee5d8b5e
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-04 11:37:27 +1000

    Add new Project Home work

---
+++ Home work/Program.cs
@@ -0,0 +1,24 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int times = 0;
+
+int count = 0;
+
+while (distance > 10) {
+    if (friend == 1) {
+        times = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else {
+        times = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance -= (firstFriendSpeed + secondFriendSpeed) * times;
+    count++;
+}
+
+Console.WriteLine(count);
